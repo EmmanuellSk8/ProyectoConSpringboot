a817162debb78b3024f470f9a0c9c276328e1f56
emmanuellsk8 <emmanuel50354@gmail.com>
2023-10-10 12:38:19 -0500

    cambio de logos, se sube index de admin

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -270,3 +270,21 @@
   new PureCounter();
 
 })()
+
+var theVideo = document.getElementById('myvideo'),
+    theTxt = document.querySelector('.txt-cont');
+
+function vidPlay(){
+  theVideo.play();
+  theVideo.setAttribute('controls', 'controls');
+  theTxt.style.opacity = "0";
+  theTxt.style.transition = "all .5s";
+  the.style.display = "none";
+}
+theVideo.onended = function(){
+  theTxt.style.opacity = "1";
+  theTxt.style.transition = "all .5s";
+  theTxt.style.display = "block";
+  theVideo.load();
+  theVideo.controls = false;
+};
